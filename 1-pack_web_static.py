b373f6f2623ab6453cd8c201961702632af79f11
NikkiE-Holberton <2495@holbertonschool.com>
2021-06-12 01:29:03 +0000

    Changing name

---
+++ 1-pack_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Fabric File """
+
+from fabric.api import local
+from datetime import datetime
+
+def do_pack():
+    """ Pack up web_static dir """
+
+    try:
+        now = datetime.now()
+
+        tarArchiveName = "web_static_" + now.strftime("%Y%m%d%H%M%S") + ".tgz"
+        tarArchivePath = "versions/" + tarArchiveName
+
+        local("mkdir -p versions")
+        local("tar -czvf " + tarArchivePath + " web_static")
+
+        return tarArchivePath
+
+    except:
+        return None
+
+    
