7c12c059b23b5beef63d8250069fd4a20700a489
NikkiE-Holberton <2495@holbertonschool.com>
2021-06-12 01:42:04 +0000

    Making PEP8 Compliant

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,27 +8,32 @@ import os.path
 
 env.hosts = ['3.80.127.172', '54.86.53.211']
 
+
 def do_deploy(archive_path):
     """Deploy an archive"""
 
     if not os.path.exists(archive_path):
         return False
-    
+
     try:
         archiveName = archive_path[9:]
-        archiveNameNoExt = archiveName [:-4]
+        archiveNameNoExt = archiveName[:-4]
 
         put(archive_path, '/tmp/test.tmp')
         run("mkdir -p /data/web_static/releases/" + archiveNameNoExt)
-        run("tar -xzvf /tmp/" + archiveName + " -C " + "/data/web_static/releases/" + archiveNameNoExt  + " --strip--components=1")
+        run("tar -xzvf /tmp/" + archiveName + " -C " +
+            "/data/web_static/releases/" +
+            archiveNameNoExt + " --strip--components=1")
         run("rm -f /tmp/" + archiveName)
         run("rm -f /data/web_static/current")
-        run("rm -sf /data/web_static/releases" + archiveNameNoExt + " /data/web_static/current")
+        run("rm -sf /data/web_static/releases" +
+            archiveNameNoExt + " /data/web_static/current")
         run("ln -sf /data/web_static/releases/" + archiveNameNoExt)
-    
+
         return True
     except:
         return False
+
 
 def do_pack():
     """ Pack up web_static dir """
